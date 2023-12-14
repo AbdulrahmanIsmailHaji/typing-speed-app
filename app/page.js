c71486ce55b3c18d23a909e04f77a0e753712652
Abdulrahman Ismail <abdulrahmanismail987@gmail.com>
2023-12-14 14:50:33 +0300

    initial

--- app/page.js
+++ app/page.js
@@ -1,95 +1,175 @@
-import Image from 'next/image'
-import styles from './page.module.css'
+"use client";
+import Image from "next/image";
+import styles from "./page.module.css";
+import { generate } from "random-words";
+import { useEffect, useRef, useState } from "react";
+const NUMB_OF_WORDS = 200;
 
 export default function Home() {
-  return (
-    <main className={styles.main}>
-      <div className={styles.description}>
-        <p>
-          Get started by editing&nbsp;
-          <code className={styles.code}>app/page.js</code>
-        </p>
-        <div>
-          <a
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className={styles.vercelLogo}
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
+  const [words, setWords] = useState([]);
+  const [secondes, setSecondes] = useState(0);
+  const [countDown, setCountDown] = useState(secondes);
+  const inputRef = useRef(null);
+  const [status, setStatus] = useState();
+  const [currInput, setCurrInput] = useState("");
+  const [currWordIndex, setCurrWordIndex] = useState(0);
+  const [currCharIndex, setCurrCharIndex] = useState(-1);
+  const [currChar, setCurrChar] = useState("");
+  const [correct, setCorrect] = useState(0);
+  const [incorrect, setIncorrect] = useState(0);
 
-      <div className={styles.center}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
+  const generateWord = () => {
+    return new Array(NUMB_OF_WORDS).fill(null).map(() => generate());
+  };
+
+  useEffect(() => {
+    setWords(generateWord());
+  }, []);
+  useEffect(() => {
+    if (status === "started") {
+      inputRef.current.focus();
+    }
+  }, [status]);
+  useEffect(() => {
+    setCountDown(secondes);
+  }, [secondes]);
+  const handleStart = () => {
+    if (status === "finished") {
+      setWords(generateWord());
+      setCurrWordIndex(0);
+      setCorrect(0);
+      setIncorrect(0);
+      setCurrCharIndex(-1);
+      setCurrChar("");
+    }
+    if (status !== "started") {
+      setStatus("started");
+      const interval = setInterval(() => {
+        setCountDown((prev) => {
+          if (prev === 0) {
+            clearInterval(interval);
+            setStatus("finished");
+            setCurrInput("");
+            return secondes;
+          } else {
+            return prev - 1;
+          }
+        });
+      }, 1000);
+    }
+  };
+
+  const handleKeyDown = ({ keyCode, key }) => {
+    if (keyCode === 32) {
+      checkMatch();
+      setCurrWordIndex(currWordIndex + 1);
+      setCurrCharIndex(-1);
+      setCurrInput("");
+    } else if (keyCode === 8) {
+      setCurrCharIndex(currCharIndex - 1);
+      setCurrChar("");
+    } else {
+      setCurrCharIndex(currCharIndex + 1);
+      setCurrChar(key);
+    }
+  };
+
+  function getCharClass(wordIdx, charIdx, char) {
+    if (
+      wordIdx === currWordIndex &&
+      charIdx === currCharIndex &&
+      currChar &&
+      status !== "finished"
+    ) {
+      if (char === currChar) {
+        return "has-background-success";
+      } else {
+        return "has-background-danger";
+      }
+    } else if (
+      wordIdx === currWordIndex &&
+      currCharIndex >= words[currWordIndex].length
+    ) {
+      return "has-background-danger";
+    } else {
+      return "";
+    }
+  }
+  const checkMatch = () => {
+    const compareWord = words[currWordIndex];
+    const match = compareWord === currInput.trim();
+    if (compareWord) {
+      setCorrect(correct + 1);
+    } else {
+      setIncorrect(incorrect + 1);
+    }
+  };
+
+  return (
+    <>
+      <div className="bt">
+        <h1>Let's know how much power do you have in typing</h1>
+        <button onClick={handleStart} disabled={secondes === 0}>
+          {" "}
+          START
+        </button>
+        <p>please enter a time that you want to practice with</p>
+        <input
+          type="number"
+          className="sec"
+          onChange={(e) => setSecondes(e.target.value)}
+          value={secondes}
+        />
+        <p>({countDown})</p>
+      </div>
+      <h1>Hurry Up Your Time Is running out</h1>
+      <div className="input-field">
+        <label htmlFor="input">Typing Here:</label>
+        <input
+          value={currInput}
+          onKeyDown={handleKeyDown}
+          ref={inputRef}
+          type="text"
+          id="input"
+          name="input"
+          onChange={(e) => setCurrInput(e.target.value)}
         />
       </div>
+      {status === "started" && (
+        <div className="paragraph">
+          {words.map((word, i) => (
+            <span key={i}>
+              <span>
+                {word.split("").map((char, idx) => (
+                  <span className={getCharClass(i, idx, char)} key={idx}>
+                    {char}
+                  </span>
+                ))}
+              </span>
+              <span> </span>
+            </span>
+          ))}
+        </div>
+      )}
 
-      <div className={styles.grid}>
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Docs <span>-&gt;</span>
-          </h2>
-          <p>Find in-depth information about Next.js features and API.</p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Learn <span>-&gt;</span>
-          </h2>
-          <p>Learn about Next.js in an interactive course with&nbsp;quizzes!</p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Templates <span>-&gt;</span>
-          </h2>
-          <p>Explore starter templates for Next.js.</p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Deploy <span>-&gt;</span>
-          </h2>
-          <p>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
-  )
+      {status === "finished" && (
+        <div className="section">
+          <div className="columns">
+            <div className="column has-text-centered">
+              {<p>Words per {secondes} secondes:</p>}
+              <p className="has-text-primary ">{correct}</p>
+            </div>
+            <div className="column has-text-centered">
+              <p>Accuracy:</p>
+              <p className="has-text-info ">
+                {correct !== 0
+                  ? `${Math.round((correct / (correct + incorrect)) * 100)}%`
+                  : "0%"}
+              </p>
+            </div>
+          </div>
+        </div>
+      )}
+    </>
+  );
 }
